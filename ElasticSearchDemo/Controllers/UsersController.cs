4d0e425d6107311fef93333762f63db161a91eab
geraldmaale <55647505+geraldmaale@users.noreply.github.com>
2021-07-25 05:00:36 +0800

    Add project files.

---
+++ ElasticSearchDemo/Controllers/UsersController.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Nest;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ElasticSearchDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IElasticClient _elasticClient;
+
+        public UsersController(IElasticClient elasticClient)
+        {
+            _elasticClient = elasticClient;
+        }
+
+        
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            var response = await _elasticClient.SearchAsync<User>(s=>s
+                .Index("users")
+                .Query(q=>q.Term(t=>t.Name, id) || 
+                          q.Match(m=>m.Field(f=>f.Name).Query(id))));
+
+            return Ok(response.Documents.FirstOrDefault());
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] User value)
+        {
+            var response = await _elasticClient.IndexAsync<User>(value, x => x.Index("users"));
+
+            return Ok(response.Id);
+        }
+
+        
+    }
+}
---
+++ ElasticSearchDemo/Controllers/WeatherForecastsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using ElasticSearchDemo.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ElasticSearchDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherForecastsController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastsController> _logger;
+
+        public WeatherForecastsController(ILogger<WeatherForecastsController> logger)
+        {
+            _logger = logger;
+        }
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                var rng = new Random();
+                if (rng.Next(0, 5) < 2)
+                {
+                    throw new Exception("Oops, what happened!!!");
+                }
+
+                var results = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                }).ToArray();
+
+                return Ok(results);
+            }
+            catch (Exception e)
+            {
+                _logger.LogCritical(e, "Server failed");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Server failed");
+            }
+        }
+    }
+}
